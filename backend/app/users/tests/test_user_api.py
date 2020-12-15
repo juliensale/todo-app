7704396a1a0eeade9b202a01d5c519e421e8745e
Julien Salé <sale.julien@orange.fr>
2020-12-15 09:11:05 +0100

    Ajout Token et PrivateUserTests

--- backend/app/users/tests/test_user_api.py
+++ backend/app/users/tests/test_user_api.py
@@ -11,7 +11,7 @@ ME_URL = reverse("users:me")
 
 
 def create_user(**params):
-    return get_user_model().objects.create(**params)
+    return get_user_model().objects.create_user(**params)
 
 
 class PublicUserApiTests(TestCase):
@@ -70,3 +70,106 @@ class PublicUserApiTests(TestCase):
         })
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_for_user(self):
+        """Test la création d'un token pour un user"""
+        payload = {
+            'username': 'test',
+            'email': 'test@email.com',
+            'password': 'testpass'
+        }
+        create_user(**payload)
+        res = self.client.post(TOKEN_URL, {
+            'email': payload['email'],
+            'password': payload['password']
+        })
+
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_create_token_invalid_credentials(self):
+        """Test création token échoue pour des arguments invalides"""
+        create_user(**{
+            'username': 'test',
+            'email': 'test@email.com',
+            'password': 'testpass'
+        })
+
+        res = self.client.post(TOKEN_URL, {
+            'username': 'test',
+            'email': 'test@email.com',
+            'password': 'wrong'
+        })
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+
+    def test_create_token_no_user(self):
+        """Test création token pour user inexistant échoue"""
+        payload = {
+            'username': 'test',
+            'email': 'test@email.com',
+            'password': 'testpass'
+        }
+
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+
+    def test_create_token_missing_field(self):
+        """Test que username, email et password sont requis"""
+        res = self.client.post(TOKEN_URL, {
+            'username': 'test',
+            'email': 'one',
+            'password': ''
+        })
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+
+    def test_retrieve_unauthorized_user(self):
+        """Test que la partie users:me est interdite"""
+        res = self.client.get(ME_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateUserApiTests(TestCase):
+
+    def setUp(self):
+        self.user = create_user(
+            username='test',
+            email='test@londonappdev.com',
+            password='testpass',
+            name='Test Name'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_user(self):
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'username': self.user.username,
+            'name': self.user.name,
+            'email': self.user.email
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test qu'on ne peut utiliser la méthode post sur users:me"""
+        res = self.client.post(ME_URL, {})
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_profile(self):
+        payload = {
+            'name': 'Test Name',
+            'password': 'newpassword123'
+        }
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
