b195409d7eff49f82a6d7bfa03567c41f38059a4
saltyozux <75158693+saltyozux@users.noreply.github.com>
2020-12-17 11:00:24 +0100

    Task (un)complete API

--- backend/app/todo/tests/test_task_api.py
+++ backend/app/todo/tests/test_task_api.py
@@ -211,3 +211,113 @@ class PrivateTaskApiTests(TestCase):
             get_detail_url(task2.id)
         )
         self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_complete_task(self):
+        """Test la complétion d'une tâche"""
+        task = Task.objects.create(
+            user=self.user,
+            sublist=self.sublist,
+            title="Ma tache"
+        )
+
+        res = self.client.put(reverse(
+            'todo:task-complete',
+            kwargs={
+                'pk': task.id
+            }
+        ))
+
+        task.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertTrue(task.completed)
+
+    def test_uncomplete_task(self):
+        """Test la dé-complétion d'une tâche"""
+        task = Task.objects.create(
+            user=self.user,
+            sublist=self.sublist,
+            title="Ma tache",
+            completed=True
+        )
+
+        res = self.client.put(reverse(
+            'todo:task-uncomplete',
+            kwargs={
+                'pk': task.id
+            }
+        ))
+
+        task.refresh_from_db()
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertFalse(task.completed)
+
+    def test_complete_limited_to_owner(self):
+        """Test qu'on ne peut completer les taches d'un autre utilisateur"""
+        user2 = get_user_model().objects.create_user(
+            username="user2",
+            email="user2@email.com",
+            password="testspassss123"
+        )
+        list2 = List.objects.create(
+            user=user2,
+            color="red",
+            title="Sa tache"
+        )
+        sublist2 = SubList.objects.create(
+            user=user2,
+            list=list2,
+            title="Sa tache"
+        )
+        task2 = Task.objects.create(
+            user=user2,
+            sublist=sublist2,
+            title="Sa tache",
+        )
+
+        res = self.client.put(reverse(
+            'todo:task-complete',
+            kwargs={
+                'pk': task2.id
+            }
+        ))
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        task2.refresh_from_db()
+        self.assertFalse(task2.completed)
+
+    def test_uncomplete_limited_to_owner(self):
+        """Test qu'on ne peut dé-completer les taches d'un autre utilisateur"""
+        user2 = get_user_model().objects.create_user(
+            username="user2",
+            email="user2@email.com",
+            password="testspassss123"
+        )
+        list2 = List.objects.create(
+            user=user2,
+            color="red",
+            title="Sa tache"
+        )
+        sublist2 = SubList.objects.create(
+            user=user2,
+            list=list2,
+            title="Sa tache"
+        )
+        task2 = Task.objects.create(
+            user=user2,
+            sublist=sublist2,
+            title="Sa tache",
+            completed=True
+        )
+
+        res = self.client.put(reverse(
+            'todo:task-uncomplete',
+            kwargs={
+                'pk': task2.id
+            }
+        ))
+
+        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        task2.refresh_from_db()
+        self.assertTrue(task2.completed)
--- backend/app/todo/views.py
+++ backend/app/todo/views.py
@@ -3,6 +3,9 @@
 from rest_framework import viewsets, mixins  # , status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 
 from todo.serializers import ListSerializer, SubListSerializer, TaskSerializer
@@ -45,3 +48,29 @@ class SubListViewSet(BaseItemViewSet):
 class TaskViewSet(BaseItemViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    @action(
+        methods=['put'],
+        detail=True,
+        permission_classes=[IsAuthenticated]
+    )
+    def complete(self, request, pk=None):
+        task = self.queryset.get(id=pk)
+        if request.user == task.user:
+            task.complete()
+            return Response(task.completed)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    @action(
+        methods=['put'],
+        detail=True,
+        permission_classes=[IsAuthenticated]
+    )
+    def uncomplete(self, request, pk=None):
+        task = self.queryset.get(id=pk)
+        if request.user == task.user:
+            task.uncomplete()
+            return Response(task.completed)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
