6a638b036bb8487426b6aa9adcb6b3470cb6cba2
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-09-10 10:21:03 +0200

    fix: Wait for material UI

--- frontend/app/pages/_document.tsx
+++ frontend/app/pages/_document.tsx
@@ -3,26 +3,26 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core'
 
 class MyDocument extends Document {
-	// static async getInitialProps(ctx: any) {
-	// 	const sheets = new ServerStyleSheets();
-	// 	const originalRenderPage = ctx.renderPage;
+	static async getInitialProps(ctx: any) {
+		const sheets = new ServerStyleSheets();
+		const originalRenderPage = ctx.renderPage;
 
-	// 	ctx.renderPage = () =>
-	// 		originalRenderPage({
-	// 			enhanceApp: (App: any) => (props: any) => sheets.collect(<App {...props} />)
-	// 		});
+		ctx.renderPage = () =>
+			originalRenderPage({
+				enhanceApp: (App: any) => (props: any) => sheets.collect(<App {...props} />)
+			});
 
-	// 	const initialProps = await Document.getInitialProps(ctx);
+		const initialProps = await Document.getInitialProps(ctx);
 
-	// 	return {
-	// 		...initialProps,
-	// 		// Styles fragment is rendered after the app and page rendering finish.
-	// 		styles: [
-	// 			...React.Children.toArray(initialProps.styles),
-	// 			sheets.getStyleElement()
-	// 		]
-	// 	};
-	// }
+		return {
+			...initialProps,
+			// Styles fragment is rendered after the app and page rendering finish.
+			styles: [
+				...React.Children.toArray(initialProps.styles),
+				sheets.getStyleElement()
+			]
+		};
+	}
 
 	render() {
 		return (
