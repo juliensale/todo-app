6ec148d3982566145656cbc1e10a8431cadea969
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-09-28 11:57:26 +0200

    fix: head media phone

--- frontend/app/components/ItemLists/Head.tsx
+++ frontend/app/components/ItemLists/Head.tsx
@@ -1,7 +1,7 @@
-import { Box, createStyles, makeStyles, Theme, Typography } from '@material-ui/core'
+import { Box, createStyles, makeStyles, Theme, Typography, useMediaQuery } from '@material-ui/core'
 import React, { FC } from 'react'
 import Link from 'next/link'
-import { ChevronRight } from '@material-ui/icons'
+import { ChevronLeft, ChevronRight } from '@material-ui/icons'
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
 	container: {
@@ -42,27 +42,46 @@ type Props = {
 }
 const Head: FC<Props> = ({ objectLinks, className = "", style = {} }) => {
 	const classes = useStyles()
+	const isMediaPhone = useMediaQuery('(max-width:700px)')
 	return (
 		<Box className={className ? `${className} classes.container` : classes.container} style={style}>
-			<ChevronRight />
 			{
-				objectLinks?.map((item, index) => (
-					item.active
-						? (
-							<Typography key={`head-${index}`} className={classes.active} >
-								{item.title}
-							</Typography>
-						) : (
-							<div key={`head-${index}`} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-								<Link href={item.href} passHref>
-									<Typography className={classes.link} >
-										{item.title}
-									</Typography>
+				isMediaPhone === false
+					? (
+						<>
+							<ChevronRight />
+							{
+								objectLinks?.map((item, index) => (
+									item.active
+										? (
+											<Typography key={`head-${index}`} className={classes.active} >
+												{item.title}
+											</Typography>
+										) : (
+											<div key={`head-${index}`} style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+												<Link href={item.href} passHref>
+													<Typography className={classes.link} >
+														{item.title}
+													</Typography>
+												</Link>
+												<ChevronRight />
+											</div>
+										)
+								))
+							}
+						</>
+					)
+					: (
+						<>
+							{objectLinks.length > 1 ?
+								<Link href={objectLinks.length > 1 ? objectLinks[objectLinks.length - 2].href : "/"}>
+									<ChevronLeft />
 								</Link>
-								<ChevronRight />
-							</div>
-						)
-				))
+								: null
+							}
+							<Typography className={classes.link}>{objectLinks[objectLinks.length - 1]?.title}</Typography>
+						</>
+					)
 			}
 		</Box>
 	)
